079e91fa7d92458c3c7562c1f330775885ef44e9
marcusvco <marcuscarvalhodeoliveira@gmail.com>
2026-03-06 20:51:23 -0300

    refactor: change sizes about section

--- src/components/about/interest-section.tsx
+++ src/components/about/interest-section.tsx
@@ -19,12 +19,12 @@ export function InterestsSection({ interests }: InterestSectionProps) {
       <img
         src={interest.image}
         alt={interest.interest}
-        className="size-56 shrink-0 object-contain sm:size-72"
+        className="size-40 shrink-0 object-contain sm:size-72"
       />
 
       <div>
-        {/* <h3 className="font-bebas-neue text-2xl font-bold">{interest}</h3> */}
-        <p className="font-instrument-sans text-center text-3xl font-extrabold tracking-tight [word-spacing:-2px] text-shadow-md sm:text-left">
+        {/* <h3 className="font-bebas-neue text-2xl font-bold">{interest.interest}</h3> */}
+        <p className="font-instrument-sans text-center text-xl font-extrabold tracking-tight [word-spacing:-2px] text-shadow-md sm:text-left sm:text-3xl">
           {interest.text}
         </p>
       </div>
