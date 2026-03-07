85d77eec069d14fe3a52126af4238f1f4fd7f31d
marcusvco <marcuscarvalhodeoliveira@gmail.com>
2026-03-06 21:32:18 -0300

    fix: change email from resend

--- src/components/contact/contact.server.ts
+++ src/components/contact/contact.server.ts
@@ -9,7 +9,7 @@ export const sendEmail = createServerFn({ method: "POST" }).handler(
     const resend = new Resend(process.env.RESEND_API_KEY)
 
     const response = await resend.emails.send({
-      from: payload.email,
+      from: process.env.VITE_EMAIL ?? "",
       to: process.env.VITE_EMAIL ?? "",
       subject: `New message from ${payload.name}`,
       html: `<p>Name: ${payload.name}</p><p>Email: ${payload.email}</p><p>Message: ${payload.message}</p>`,
